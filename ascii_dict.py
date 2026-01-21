26c8b36776e2cac526441d318c0c4d7272ff218d
xtsebas <thiagohugo2014@gmail.com>
2026-01-20 19:33:51 -0600

    feat: ascii to binary done

---
+++ ascii_dict.py
@@ -0,0 +1,1 @@
+ASCII_TO_BINARY = {chr(i): format(i, '08b') for i in range(128)}
--- codigos.py
+++ codigos.py
@@ -1,5 +1,13 @@
-def asciiToBinary():
-    pass
+from ascii_dict import ASCII_TO_BINARY
+
+
+def asciiToBinary(text):
+    text = text.lower()
+    binary_result = ""
+    for char in text:
+        if char in ASCII_TO_BINARY:
+            binary_result += ASCII_TO_BINARY[char]
+    return binary_result
 
 def base64ToBinary():
     pass
@@ -9,3 +17,8 @@ def binaryToAscii():
 
 def binaryToBase64():
     pass
+
+
+text = "Hello World"
+binary_output = asciiToBinary(text)
+print(binary_output)  
