c58e73aa6ed8ea23eb0d14059b5c88e8f69338a3
xtsebas <thiagohugo2014@gmail.com>
2026-01-22 18:23:20 -0600

    feat: base64 to binary

--- codigos.py
+++ codigos.py
@@ -39,16 +39,58 @@ def textToBinary(text):
         binary_result += asciiToBinary(ascii_value)
     return binary_result
 
-def base64ToBinary():
-    pass
+def charToBase64Value(char):
+    base64_map = {
+        'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7,
+        'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15,
+        'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21, 'W': 22, 'X': 23,
+        'Y': 24, 'Z': 25, 'a': 26, 'b': 27, 'c': 28, 'd': 29, 'e': 30, 'f': 31,
+        'g': 32, 'h': 33, 'i': 34, 'j': 35, 'k': 36, 'l': 37, 'm': 38, 'n': 39,
+        'o': 40, 'p': 41, 'q': 42, 'r': 43, 's': 44, 't': 45, 'u': 46, 'v': 47,
+        'w': 48, 'x': 49, 'y': 50, 'z': 51, '0': 52, '1': 53, '2': 54, '3': 55,
+        '4': 56, '5': 57, '6': 58, '7': 59, '8': 60, '9': 61, '+': 62, '/': 63
+    }
+    return base64_map.get(char, -1)
 
-def binaryToAscii():
+
+def base64ValueToBinary(value):
+    if value < 0:
+        return ''
+
+    if value == 0:
+        return '000000'
+
+    binary = ''
+    temp = value
+
+    while temp > 0:
+        residuo = temp % 2
+        binary = str(residuo) + binary
+        temp = temp // 2
+
+    while len(binary) < 6:
+        binary = '0' + binary
+
+    return binary
+
+
+def base64ToBinary(base64_text):
+    binary_result = ""
+    for char in base64_text:
+        if char == '=':  
+            continue
+        base64_value = charToBase64Value(char)
+        binary_result += base64ValueToBinary(base64_value)
+    return binary_result
+
+def binaryToAscii(number):
+    
     pass
 
 def binaryToBase64():
     pass
 
 
-text = "Hello World"
-binary_output = textToBinary(text)
+text = "VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIDEzIGxhenkgZG9ncy4="
+binary_output = base64ToBinary(text)
 print(binary_output)  
