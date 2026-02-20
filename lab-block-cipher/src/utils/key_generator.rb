3d7fd8fe69ae347307f55686c6fb38c5eb249056
xtsebas <thiagohugo2014@gmail.com>
2026-02-19 18:30:56 -0600

    feat: generator keys done

--- lab-block-cipher/src/utils/key_generator.rb
+++ lab-block-cipher/src/utils/key_generator.rb
@@ -12,14 +12,23 @@ end
 def generate_3des_key(key_option: 2)
   # Genera una clave 3DES aleatoria.
   
-  return true
+  case key_option
+    when 2 then SecureRandom.bytes(16)
+    when 3 then SecureRandom.bytes(24)
+    else raise ArgumentError, "key_option debe ser 2 o 3"
+  end
 end
 
 def generate_aes_key(key_size: 256)
   # TODO: Implementar
   # Convertir bits a bytes: key_size // 8
+  
+  raise ArgumentError, "key_size debe ser 128, 192 o 256" unless [128, 192, 256].include?(key_size)
+  SecureRandom.bytes(key_size / 8)
 end
 
 def generate_iv(block_size: 8)
-  # TODO: Implementar
+  # Genera un IV aleatorio criptograficamente seguro
+
+  SecureRandom.bytes(block_size)
 end
